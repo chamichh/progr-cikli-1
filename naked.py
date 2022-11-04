b435cae4c699166839bea03da97d3acbe2aa3ebe
Aleksis <aleksis.kalis02@gmail.com>
2022-11-04 22:18:07 +0000

    Added external config reading

--- naked.py
+++ naked.py
@@ -5,14 +5,28 @@ import time
 import yaml
 
 from datetime import datetime
+from configparser import ConfigParser
+
 print('Asteroid processing service')
 
 # Initiating and reading config values
 print('Loading configuration from file')
 
-# Preset nasa site and API key.
-nasa_api_key = "???"
-nasa_api_url = "https://api.nasa.gov/neo/"
+try:
+	config = ConfigParser()
+	config.read('config.ini')
+
+	nasa_api_key = config.get('nasa', 'api_key')
+	nasa_api_url = config.get('nasa', 'api_url')
+
+	mysql_config_mysql_host = config.get('mysql_config', 'mysql_host')
+	mysql_config_mysql_db = config.get('mysql_config', 'mysql_db')
+	mysql_config_mysql_user = config.get('mysql_config', 'mysql_user')
+	mysql_config_mysql_pass = config.get('mysql_config', 'mysql_pass')
+
+except:
+	logger.exception('')
+print.info('DONE')
 
 # Getting todays date
 dt = datetime.now()
